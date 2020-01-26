98c08a0f7c21b972b6200ff923f3f4102afa952d
Jeffrey Freeman <cisigep@gmail.com>
2020-01-26 15:18:54 -0500

    thymeleaf test and cors add to test Angular requests

--- src/main/java/io/cisigep/easynotes/controllers/NotesController.java
+++ src/main/java/io/cisigep/easynotes/controllers/NotesController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import io.cisigep.easynotes.exceptions.ResourceNotFoundException;
 import io.cisigep.easynotes.model.Note;
 import io.cisigep.easynotes.repositories.NoteRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class NotesController {
